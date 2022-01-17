910ef284d254a220aaae0fe5266c5be6e0d69006
cryptosgn <john.jentges@gmail.com>
2022-01-17 23:14:08 +0100

    somewhat working
    Die Matritzen funktionieren, das Laden der Texturen, damit die Darstellung eines "sinvollen" Objektes allerdings noch nicht.

--- src/main/Cam.java
+++ src/main/Cam.java
@@ -1,26 +1,48 @@
 package main;
 
+import java.util.HashMap;
+
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.joml.Vector2f;
 
 public class Cam {
 	
-    private Matrix4f projectionMatrix, viewMatrix;
+	protected static HashMap<String, Integer> uniforms;
+    private Matrix4f projectionMatrix, viewMatrix;;
     public Vector2f position;
-    private static final float FOV = (float) Math.toRadians(60.0f);
-	private static final float aspectRatio = 1920.0f / 1080.0f;
+    private static final float FOV = 70f;
+    private static final float NEAR_PLANE = 0.1f;
+    private static final float FAR_PLANE = 1000.0f;
+
 
     public Cam(Vector2f position) {
+    	uniforms = new HashMap<>();
         this.position = position;
         this.projectionMatrix = new Matrix4f();
         this.viewMatrix = new Matrix4f();
         adjustProjection();
+//        System.out.println(projectionMatrix);
+//        System.out.println(viewMatrix);
     }
 
     public void adjustProjection() {
-    	projectionMatrix = new Matrix4f().perspective(FOV, aspectRatio,
-    	0.01f, 1000.0f);
+        float width = 1920f;
+        float height = 1080f;
+        float aspectRatio = width / height;
+        float yScale = (float) ((1f / Math.tan(Math.toRadians(FOV / 2f))) * aspectRatio);
+        float xScale = yScale / aspectRatio;
+        float frustumLength = FAR_PLANE - NEAR_PLANE;
+        
+        projectionMatrix = new Matrix4f();
+        projectionMatrix.m00(xScale);
+        projectionMatrix.m11(yScale);
+        projectionMatrix.m22(-((FAR_PLANE + NEAR_PLANE) / frustumLength));
+        projectionMatrix.m23(-1);
+        projectionMatrix.m32(-((2 * NEAR_PLANE * FAR_PLANE) / frustumLength));
+        projectionMatrix.m33(0);
+        
+//        System.out.println(projectionMatrix);
         
     }
     
@@ -31,8 +53,7 @@ public class Cam {
         this.viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f),
                                         cameraFront.add(position.x, position.y, 0.0f),
                                         cameraUp);
-//        In OGL eine Scale-Methode, sowie eine Pitch- und Yaw-Methode finden
-
+        
         return this.viewMatrix;
     }
 
--- src/main/LvlEdit.java
+++ src/main/LvlEdit.java
@@ -7,50 +7,74 @@ import org.joml.Matrix4f;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL46;
+import org.lwjgl.openvr.Texture;
+import org.lwjgl.system.MemoryStack;
+
+import util.Textures;
 
 public class LvlEdit extends Scene {
 	
 	private String vertexShaderSrc = "#version 460\r\n"
 			+ "layout (location=0) in vec3 aPos;\r\n"
 			+ "layout (location=1) in vec4 aColor;\r\n"
+			+ "layout (location=2) in vec2 aTexCoords;\r\n"
 			+ "\r\n"
 			+ "uniform mat4 uProj;\r\n"
 			+ "uniform mat4 uView;\r\n"
 			+ "\r\n"
 			+ "out vec4 fColor;\r\n"
+			+ "out vec2 fTexCoords;\r\n"
 			+ "\r\n"
 			+ "void main()\r\n"
 			+ "{\r\n"
 			+ "    fColor = aColor;\r\n"
+			+ "    fTexCoords = aTexCoords;\r\n"
 			+ "    gl_Position = uProj * uView * vec4(aPos, 1.0);\r\n"
 			+ "}";
 	
 	private String fragmentShaderSrc = "#version 460\r\n"
 			+ "\r\n"
+			+ "uniform float uTime;\r\n"
+			+ "uniform sampler2D TEX_SAMPLER;\r\n"
+			+ "\r\n"
 			+ "in vec4 fColor;\r\n"
+			+ "in vec2 fTexCoords;\r\n"
 			+ "\r\n"
 			+ "out vec4 color;\r\n"
 			+ "\r\n"
 			+ "void main()\r\n"
 			+ "{\r\n"
-			+ "    color = fColor;\r\n"
+			+ "    color = texture(TEX_SAMPLER, fTexCoords);\r\n"
 			+ "}";
 	
 	private float[] vertexArr = {
 //			position			color
 //			x,y,z				r,g,b,a
-	         100.5f, 0.5f, 0.0f,       1.0f, 0.0f, 0.0f, 1.0f, // Bottom right 0
-	         0.5f,  100.5f, 0.0f,       0.0f, 1.0f, 0.0f, 1.0f, // Top left     1
-	          100.5f,  100.5f, 0.0f ,      1.0f, 0.0f, 1.0f, 1.0f, // Top right    2
-	         0.5f, 0.5f, 0.0f,       1.0f, 1.0f, 0.0f, 1.0f // Bottom left  3
-	         //Mehr nicht mï¿½glich wegen idexierung
+		    -0.5f,  0.5f,  0.5f,	0.5f, 0.0f, 0.0f, 1.0f,
+		    -0.5f, -0.5f,  0.5f,	0.0f, 0.5f, 0.0f, 1.0f,
+		    0.5f, -0.5f,  0.5f,		0.0f, 0.0f, 0.5f, 1.0f,
+		    0.0f, 0.0f, 0.5f,		0.0f, 0.5f, 0.5f, 1.0f,
+		    -0.5f,  0.5f, -0.5f,	0.5f, 0.0f, 0.0f, 1.0f,
+		    0.5f,  0.5f, -0.5f,		0.0f, 0.5f, 0.0f, 1.0f,
+		    -0.5f, -0.5f, -0.5f,	0.0f, 0.0f, 0.5f, 1.0f,
+		    0.5f, -0.5f, -0.5f,	    0.0f, 0.5f, 0.5f, 1.0f
 	};
 	
 //	+++Wichtig+++ muss gegen den Uhrzeigersinn verlaufen
 	private int[] elementArr = {
 			
-            2, 1, 0, // Top right triangle
-            0, 1, 3 // bottom left triangle
+		    // Front face
+		    0, 1, 3, 3, 1, 2,
+		    // Top Face
+		    4, 0, 3, 5, 4, 3,
+		    // Right face
+		    3, 2, 7, 5, 3, 7,
+		    // Left face
+		    6, 1, 0, 6, 0, 4,
+		    // Bottom face
+		    2, 1, 6, 2, 6, 7,
+		    // Back face
+		    7, 6, 4, 7, 4, 5
 			
 	};
 	
@@ -60,9 +84,11 @@ public class LvlEdit extends Scene {
 	
 	private int VAOID, VBOID, EBOID;
 	
+	private Textures testTexture;
 	
 	@Override
 	public void init() {
+		
 		this.ZweiundVierzig = new Cam(new Vector2f());
 //		Shader compilen und linken	
 //		Shader laden und compilen
@@ -103,6 +129,8 @@ public class LvlEdit extends Scene {
 			
 		assert false: "";
 		}
+		
+		this.testTexture = new Textures("Textures/testImage.png");
 		
 //		Shader linken
 		ShaderProg = GL46.glCreateProgram();
@@ -152,12 +180,16 @@ public class LvlEdit extends Scene {
 	@Override
 	public void update(float deltaT) {
 		
-        ZweiundVierzig.position.x -= deltaT * 50.0f;
-        ZweiundVierzig.position.y -= deltaT * 20.0f;
+		// Update rotation angle
+//		float rotation = gameItem.getRotation().x + 1.5f;
+//		if ( rotation > 360 ) {
+//		    rotation = 0;
+//		}
+//		gameItem.setRotation(rotation, rotation, rotation);
 		
-		uploadMat4f("uProj", ZweiundVierzig.getProjectionMatrix());
-		uploadMat4f("uView", ZweiundVierzig.getViewMatrix());
-		
+//        ZweiundVierzig.position.x -= deltaT * 50.0f;
+//        ZweiundVierzig.position.y -= deltaT * 20.0f;
+
 		 // Bind shader program
         GL46.glUseProgram(ShaderProg);
         // Bind the VAO that we're using
@@ -166,6 +198,25 @@ public class LvlEdit extends Scene {
         // Enable the vertex attribute pointers
         GL46.glEnableVertexAttribArray(0);
         GL46.glEnableVertexAttribArray(1);
+        
+//        kann in späteren Versionen ausgelagert werden
+		try {
+			createUniform("uProj");
+			createUniform("uView");
+//			createUniform("uTime");
+			createUniform("TEX_SAMPLER");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        setUniform("uProj", ZweiundVierzig.getProjectionMatrix());
+        setUniform("uView", ZweiundVierzig.getViewMatrix());
+//        setUniformfloat("uView", 1.0f);  
+        
+        uploadTexture("TEX_SAMPLER", 0);
+        GL46.glActiveTexture(GL46.GL_TEXTURE0);
+        testTexture.bind();
         
 //        Wireframe-mode
         GL46.glPolygonMode(GL46.GL_FRONT_AND_BACK, GL46.GL_LINE);
@@ -185,10 +236,38 @@ public class LvlEdit extends Scene {
 		
 	}
 	
-    public void uploadMat4f(String varName, Matrix4f mat4) {
-        int varLocation = GL46.glGetUniformLocation(ShaderProg, varName);
-        FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16); //4x4=16
-        mat4.get(matrixBuffer);
-        GL46.glUniformMatrix4fv(varLocation, false, matrixBuffer);
+    	public void createUniform(String uniformName) throws Exception {
+    	    int uniformLocation = GL46.glGetUniformLocation(ShaderProg,
+    	        uniformName);
+    	    if (uniformLocation == -1) {
+    	        throw new Exception("Could not find uniform:" +
+    	            uniformName);
+    	    }
+    	    Cam.uniforms.put(uniformName, uniformLocation);
     }
+    	
+    	public void setUniform(String uniformName, Matrix4f value) {
+    	    // Dump the matrix into a float buffer
+    	    try (MemoryStack stack = MemoryStack.stackPush()) {
+    	        FloatBuffer fb = stack.mallocFloat(16);
+    	        value.get(fb);
+//    	        System.out.println(Cam.uniforms.get(uniformName));
+    	        GL46.glUniformMatrix4fv(Cam.uniforms.get(uniformName), false, fb);
+    	    }
+    	}
+    	
+    	public void setUniformfloat(String uniformName, float value) {
+    	    // Dump the matrix into a float buffer
+    	    try (MemoryStack stack = MemoryStack.stackPush()) {
+    	        FloatBuffer fb = stack.mallocFloat(1);
+//    	        System.out.println(Cam.uniforms.get(uniformName));
+    	        GL46.glUniformMatrix4fv(Cam.uniforms.get(uniformName), false, fb);
+    	    }
+    	}
+    	
+        public void uploadTexture(String varName, int slot) {
+            int varLocation = GL46.glGetUniformLocation(ShaderProg, varName);
+            GL46.glUniform1i(varLocation, slot);
+        }
+	
 }
